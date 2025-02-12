53ee58a6e7d5a953b4f3e8731bc22ad80c60ae0f
levinjoey@gmail.com <levinjoey@gmail.com>
2025-02-12 12:59:12 +0200

    Initial Commit

---
+++ Set/SetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetApp.swift
+//  Set
+//
+//  Created by joey levin on 2/12/25.
+//
+
+import SwiftUI
+
+@main
+struct SetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SetTests/SetTests.swift
@@ -0,0 +1,17 @@
+//
+//  SetTests.swift
+//  SetTests
+//
+//  Created by joey levin on 2/12/25.
+//
+
+import Testing
+@testable import Set
+
+struct SetTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
