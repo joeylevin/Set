110e88baf977512e957d623ea146e0ccd4ea4aa6
levinjoey@gmail.com <levinjoey@gmail.com>
2025-02-12 19:42:05 +0200

    Initial version of SetView Model with placehodler functions

---
+++ Set/SetViewModel.swift
@@ -0,0 +1,41 @@
+//
+//  SetViewModel.swift
+//  Set
+//
+//  Created by joey levin on 2/12/25.
+//
+import SwiftUI
+
+class SetViewModel: ObservableObject {
+    typealias Card = SetModel.Card
+
+    init() {
+        let theme = ThemeChooser().themes.randomElement()!
+        chosenTheme = theme
+        model = SetModel()
+        { (colorIndex, shapeIndex, numberIndex, shadingIndex) in
+            return Card(color: theme.colorList[colorIndex], shape: theme.shapeList[shapeIndex], number: numberIndex+1, shading: shadingIndex, id: UUID().uuidString)
+        }
+        model.shuffle()
+        print(model)
+    }
+    
+    private var chosenTheme: ThemeChooser.Theme
+    @Published private var model : SetModel
+    
+    var cards: Array<Card> {
+        return model.cards
+    }
+    
+    var theme: ThemeChooser.Theme {
+        self.chosenTheme
+    }
+    
+    func newGame() {
+        return
+    }
+    
+    func choose(_ card: Card) {
+        return
+    }
+}
